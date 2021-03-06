9ad25f3bacbe8d551736fe353dd7e8931a9b0f24
wuykuan <woyongkuan@163.com>
2021-03-07 01:03:09 +0800

    提交common

---
+++ src/common/daza/common/Card.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="card">
+		<view class="">
+			-
+		</view>
+		
+		<slot>
+			<view class="card-name">
+			{{cardTitle}}
+		    </view>
+		</slot>
+		<view class="">
+			-
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			cardTitle: String
+		}
+	}
+</script>
+
+<style scoped>
+	.card{
+		padding: 20rpx 0;
+		display: flex;
+		justify-content: center;
+		font-weight: bold;
+	}
+	.card-name{
+		padding: 0 20rpx;
+		font-size: 32rpx;
+	}
+</style>
